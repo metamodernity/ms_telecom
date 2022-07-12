e0f90f496a13bd6b383a3ce137281accc202df85
metamodernity <j772115@gmail.com>
2022-07-12 16:55:25 +0400

    First commit

---
+++ darkmode.js
@@ -0,0 +1,10 @@
+function fun1() {
+    var chbox;
+    chbox=document.getElementById('one');
+        if (chbox.checked) {
+            alert('Выбран');
+        }
+        else {
+            alert ('Не выбран');
+        }
+    }
